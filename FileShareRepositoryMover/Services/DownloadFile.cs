033a35e65d5394c92529a099b8e1999367baf533
Bryan Smith <bsmith@hivenetworks.com>
2020-05-13 00:32:07 -0400

    Updated DownloadFiles

--- FileShareRepositoryMover/Services/DownloadFile.cs
+++ FileShareRepositoryMover/Services/DownloadFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -18,7 +19,9 @@ namespace FileShareRepositoryMover.Services
 
             string folderPath = "C:\\Temp\\Mover";
             string fileUrl = System.Configuration.ConfigurationManager.AppSettings["Website"].ToString();
+            string loginUrl = System.Configuration.ConfigurationManager.AppSettings["Website"].ToString();
             fileUrl = @"https://" + fileUrl + @"/index.php?option=com_easysocial&view=groups&layout=preview&fileid=" + fileId + @"&tmpl=component";
+            loginUrl = @"https://" + loginUrl + @"/index.php/login";
 
             if (!Directory.Exists(folderPath))
             {
@@ -30,12 +33,18 @@ namespace FileShareRepositoryMover.Services
             fileData.FilePath = folderPath + "\\" + blobFileName;
             fileData.Url = fileUrl;
 
+
             try
             {
                 using (WebClient client = new WebClient())
                 {
-                    string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + password));
-                    client.Headers[HttpRequestHeader.Authorization] = $"Basic {credentials}";
+                    NameValueCollection values = new NameValueCollection
+                    {
+                        { "username", userName },
+                        { "password", password }
+                    };
+                    client.UploadValues(new Uri(loginUrl), "POST", values);
+
                     client.DownloadFile(fileUrl, fileData.FilePath);
                 }
             }
